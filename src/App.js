e77d7688183f93ed8577ef3ab4fb66f1b38ba274
ChetanSingh14 <siradhanachetan14@gmail.com>
2024-03-07 15:18:44 +0530

    ytiuftyu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+export default function 
+() {
+  return (
+    <div>
+
+      <Button variant="text">Text</Button>
+<Button variant="contained">Contained</Button>
+<Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
